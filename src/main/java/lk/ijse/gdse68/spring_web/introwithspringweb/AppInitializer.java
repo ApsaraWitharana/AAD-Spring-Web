7ad2c55a037d91729d9469bdaa53ee5c38ba2df2
ApsaraWitharana <asachini095@gmail.com>
2024-08-11 12:58:05 +0530

    update-strucher

---
+++ src/main/java/lk/ijse/gdse68/spring_web/introwithspringweb/AppInitializer.java
@@ -0,0 +1,41 @@
+package lk.ijse.gdse68.spring_web.introwithspringweb;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import lk.ijse.gdse68.spring_web.introwithspringweb.config.WebAppConfig;
+import lk.ijse.gdse68.spring_web.introwithspringweb.config.WebAppRootConfig;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+//class name has all working types
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        //config class 2kt wadiy tiynn puluwan -root or not root
+        return new Class[]{WebAppRootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebAppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"}; //what is the mapping default mapping set  =>dispatcher servlet kranwa
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//boll class appinit class //webapp-boll,/annotation ==>servlte ewa req.resp handling
---
+++ src/main/java/lk/ijse/gdse68/spring_web/introwithspringweb/config/WebAppConfig.java
@@ -0,0 +1,4 @@
+package lk.ijse.gdse68.spring_web.introwithspringweb.config;
+
+public class WebAppConfig {
+}
