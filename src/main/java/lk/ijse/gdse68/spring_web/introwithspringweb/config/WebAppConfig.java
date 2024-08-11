3fe158ce8174c7ba5664781b5b1ee990fe8c088d
ApsaraWitharana <asachini095@gmail.com>
2024-08-11 14:08:53 +0530

    update-done

--- src/main/java/lk/ijse/gdse68/spring_web/introwithspringweb/config/WebAppConfig.java
+++ src/main/java/lk/ijse/gdse68/spring_web/introwithspringweb/config/WebAppConfig.java
@@ -4,6 +4,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages ="lk.ijse.gdse68.spring_web.introwithspringweb" )
+@ComponentScan(basePackages ="lk.ijse.gdse68.spring_web.introwithspringweb" )//root package and sub package object to manage spring
 public class WebAppConfig {
 }
--- src/main/java/lk/ijse/gdse68/spring_web/introwithspringweb/controller/DemoController.java
+++ src/main/java/lk/ijse/gdse68/spring_web/introwithspringweb/controller/DemoController.java
@@ -4,11 +4,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/demo") //
-@RestController
+@RestController //meta annotation has controller annotation as this annotation has component annotation so this annotation as the managing to spring
+//@Controller- return model SSP/SSR frontend manage krnn ui define krgnn  use we //set frontend to using this
+
+//Rest API - ui note return,
 public class DemoController {
     //servlet project ek controller ekk krnwa req,resp handle krnn spring mgin
     @RequestMapping(value = "/health")
     public String healthCheck(){
-        return "DemoController run perfectly ";
+        return "DemoController run perfectly !!!";
     }
 }
+
+
+//Spring MVC Framework:
+
+//The class is annotated with @RestController, which is a Spring MVC annotation that indicates this class will handle HTTP requests and return data directly as the response body (usually JSON).
+//The @RequestMapping("/demo") annotation at the class level specifies that this controller will handle all requests that start with /demo.
+//The method healthCheck() is mapped to the /demo/health endpoint via @RequestMapping(value = "/health"). When a request is made to /demo/health, this method is invoked.
